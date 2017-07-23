4cacadf27ae0161e2c951c4060a86814280c5203
Gaya3ram <30343436+Gaya3ram@users.noreply.github.com>
2017-07-23 11:51:37 +0530

    vow.c

---
+++ vow.c
@@ -0,0 +1,13 @@
+include<stdio.h>
+int main()
+{
+int c,l,r,a,e,i,o,u,A,E,I,O,U;
+printf("enter the sentence");
+scanf("%c",&c);
+l=(c ==a||c == e|| c ==i| c == o|| c ==u); 
+r=(c ==A||c == E|| c ==I| c == O|| c ==U);
+if(l||r)
+printf("vowels");
+printf("consonent");
+return 0;
+}
