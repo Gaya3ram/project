fe519f95450487f5f77d8c31f2861b70fd1284c7
Gaya3ram <30343436+Gaya3ram@users.noreply.github.com>
2017-07-23 15:02:58 +0530

    lar.c

--- lar.c
+++ lar.c
@@ -3,12 +3,13 @@ int main()
 {
 int a,b,c;
 printf("Enter three no:");
-scanf("%nd %d %d",&a,&b,&c);
-if(a>= b ||a<= c)
+scanf("%d %d %d",&a,&b,&c);
+if(a>= b &&a>= c)
 printf(" a is large");
-if(b>= a || b<= c)
+if(b>= a && b>= c)
 printf(" b is large");
 else
+if(c<=a&&c<=b)
 printf(" c is large");
 return 0;
 }
