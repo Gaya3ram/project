e7ff479b43e57012e2454a37b546970dc120b438
Gaya3ram <30343436+Gaya3ram@users.noreply.github.com>
2017-07-24 09:50:24 +0530

    nat.c

--- nat.c
+++ nat.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
-int masum=0,n;
+int main()
+{
+sum=0,n;
 printf("enter the no");
 scanf("%d",&n);
 for(i=1;i<=n;i++)
-sum=in()
 {
-int i,sum+i;
+sum=sum+i;
 printf("sum=%d",sum);
 return 0;
 }
